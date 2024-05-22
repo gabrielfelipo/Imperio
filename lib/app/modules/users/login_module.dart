dd239ef19759dd9d97e04ddbc7cea45fde8d38f0
Felipo <gabriel.felipo2010@gmail.com>
2024-05-22 11:55:50 -0300

    feature(login): Injectable variables from the repository, datasource and usecase were created and injected into the login module

--- lib/app/modules/users/login_module.dart
+++ lib/app/modules/users/login_module.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:imperio/app/modules/users/data/datasources/login_datasource.dart';
 import 'package:imperio/app/modules/users/data/repositories/login_repository_impl.dart';
+import 'package:imperio/app/modules/users/domain/repositories/login_repository.dart';
 import 'package:imperio/app/modules/users/domain/usecases/login_usecase_impl.dart';
 import 'package:imperio/app/modules/users/external/datasources/login_datasource_impl.dart';
 import 'package:imperio/app/modules/users/presentation/pages/login/login_page.dart';
@@ -7,9 +9,13 @@ import 'package:imperio/app/modules/users/presentation/pages/login/login_page.da
 class LoginModule extends Module {
   @override
   void binds(i) {
-    // i.addSingleton(LoginDataSourceImpl.new);
-    // i.addSingleton(LoginRepositoryImpl.new);
-    // i.addSingleton(LoginUseCaseImpl.new);
+    i.addSingleton<LoginDataSource>(() => LoginDataSourceImpl());
+    i.addSingleton<LoginRepository>(
+      () => LoginRepositoryImpl(Modular.get<LoginDataSource>()),
+    );
+    i.addSingleton<LoginUseCaseImpl>(
+      () => LoginUseCaseImpl(Modular.get<LoginRepository>()),
+    );
   }
 
   @override
